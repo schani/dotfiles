8165366c56922d62b99f8fc2b6ba06a8fbeea89b
Mark Probst <mark.probst@gmail.com>
2021-09-08 17:34:54 -0700

    More fixes

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@
 
 echo "Setting up dotfiles"
 
-. profile.brew
-
-sudo apt-get install zile tig fzf
-brew install lazygit gh
-
 cat profile.brew profile >>~/.profile
 cp gitconfig ~/.gitconfig
+
+. profile.brew
+
+sudo apt-get -y install zile tig fzf
+brew install lazygit gh
+
